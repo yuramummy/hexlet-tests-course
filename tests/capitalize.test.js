5987a9eec50c8252f951f9b13b2d4674e468351c
Юрий Шершнев <yuramummy@MacBook-Pro-Urij.local>
2022-09-16 15:30:51 +0300

    cange chapitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,16 @@
+import { strict as assert } from 'assert';
 import capitalize from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
+/* if (capitalize('hello') !== 'Hello') {
     throw new Error('Функция работает неверно!');
 }
   
 if (capitalize('') !== '') {
     throw new Error('Функция работает неверно!');
 }
-  
+*/
+
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
+
 console.log('Все тесты пройдены!');
